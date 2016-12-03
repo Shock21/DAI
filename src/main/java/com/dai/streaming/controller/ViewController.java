c0557509ba6c5072347bf855cd29aa3b6f75b7e5
Shock21 <tiberiu_alx@yahoo.com>
2016-12-03 17:10:49 +0200

    Added byte-range support

--- src/main/java/com/dai/streaming/controller/ViewController.java
+++ src/main/java/com/dai/streaming/controller/ViewController.java
@@ -1,5 +1,6 @@
 package com.dai.streaming.controller;
 
+import com.dai.streaming.utils.MultipartFileSender;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
@@ -30,18 +31,12 @@ public class ViewController  {
     @RequestMapping(value = "/audio", method = RequestMethod.GET)
     @ResponseBody
     public void playAudio(HttpServletRequest request, HttpServletResponse response) {
+        String path = "E:\\Disclosure-Help_Me_Lose_My_Mind.mp3";
+        File file = new File(path);
         try {
-            String path = "D:\\Disclosure-Help_Me_Lose_My_Mind.mp3";
-            File file = new File(path);
-            response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-            response.setHeader("Content-Disposition", "attachment; filename="+file.getName().replace(" ", "_"));
-            InputStream iStream = new FileInputStream(file);
-            IOUtils.copy(iStream, response.getOutputStream());
-            response.flushBuffer();
-        } catch (java.nio.file.NoSuchFileException e) {
-            response.setStatus(HttpStatus.NOT_FOUND.value());
+            MultipartFileSender.fromFile(file).with(request).with(response).serveResource();
         } catch (Exception e) {
-            response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            e.printStackTrace();
         }
     }
 
