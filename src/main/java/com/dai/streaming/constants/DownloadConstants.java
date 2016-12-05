88a3f4b11dc52273f28562ee769582dc64100520
Tiberiu Vasile <tvasile@endava.com>
2016-12-05 10:36:40 +0200

    Squashed commit of the following:
    commit c0557509ba6c5072347bf855cd29aa3b6f75b7e5
    Author: Shock21 <tiberiu_alx@yahoo.com>
    Date:   Sat Dec 3 17:10:49 2016 +0200
    
        Added byte-range support

---
+++ src/main/java/com/dai/streaming/constants/DownloadConstants.java
@@ -0,0 +1,15 @@
+package com.dai.streaming.constants;
+
+/**
+ * Created by Tiberiu on 12/3/2016.
+ */
+
+public class DownloadConstants {
+
+    public static final String INPUT_STREAM = "inputStream";
+    public static final String CONTENT_TYPE = "contentType";
+
+    public static final String CONTENT_LENGTH = "contentLength";
+    public static final String FILENAME = "filename";
+    public static final String LAST_MODIFIED = "lastModified";
+}
