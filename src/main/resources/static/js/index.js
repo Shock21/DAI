d8be05b580adadfebe9b0952f265dfb791755cc0
Shock21 <tiberiu_alx@yahoo.com>
2017-01-09 01:17:05 +0200

    Finished auto completion function.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,22 +1,30 @@
 var app = angular.module('app', ['ngSanitize', 'MassAutoComplete']);
 app.controller('mainCtrl', function ($scope, $http, $sce, $q) {
     $scope.dirty = {};
+    $scope.tags = [];
+
+    function add_tag(selected) {
+        $scope.tags.push(selected.value);
+        $scope.dirty = {};
+    }
 
     function suggest_state(term) {
-        var results = [];
-
-        $http
-            .post(
-                '/search',
-                {'contains': term.toLowerCase().trim()}
-            )
+        $scope.dirty.term = term;
+        return $http.post('/search',{'contains': term.toLowerCase().trim()}            )
             .then(function (response) {
+                var results = [];
 
+                angular.forEach(response.data.songNames, function(song) {
+                    results.push({label : $sce.trustAsHtml(song), value : song});
+                });
+
+                return results;
             });
 
     }
 
     $scope.autocomplete_options = {
-        suggest: suggest_state
+        suggest: suggest_state,
+        on_select: add_tag
     };
 });
